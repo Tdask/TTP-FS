10230ad26fcc7345415e526ccd1c20f42f9ce8b4
Logan Takahashi <logan.takahashi@gmail.com>
2019-07-12 05:43:53 -0400

    auth routes

--- server/index.js
+++ server/index.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-express")(session.Store);
+const dbStore = new SequelizeStore({ db: db });
+const passport = require("passport");
 const app = express();
 const models = require("./db/models");
 const path = require("path");
@@ -16,9 +20,36 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.use("/api", require("./api"));
+app.use("/auth", require("./auth"));
 
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "a wildly insecure secret",
+    store: dbStore,
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  try {
+    done(null, user.id);
+  } catch (err) {
+    done(err);
+  }
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => done(null, user))
+    .catch(done);
 });
 
 app.use(function(err, req, res, next) {
